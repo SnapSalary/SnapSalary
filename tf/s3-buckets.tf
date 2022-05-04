aab5c9df9f8e7f34250b599314b20bded3c38c84
PDXChristian <8883699+PDXChristian@users.noreply.github.com>
2022-05-04 16:26:05 -0700

    ran terraform fmt

--- tf/s3-buckets.tf
+++ tf/s3-buckets.tf
@@ -7,8 +7,8 @@ resource "aws_s3_bucket" "frontend_root" {
 }
 
 resource "aws_s3_bucket_public_access_block" "block_frontend" {
-  bucket = aws_s3_bucket.frontend.bucket
-  block_public_acls = true
+  bucket              = aws_s3_bucket.frontend.bucket
+  block_public_acls   = true
   block_public_policy = true
 }
 
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
 
 data "aws_iam_policy_document" "iam_policy" {
   statement {
-    sid = "1"
+    sid     = "1"
     actions = ["S3:GetObject"]
     resources = [
       "arn:aws:s3:::www.${var.AWS_S3_BUCKET}/*",
@@ -44,10 +44,10 @@ resource "aws_s3_bucket_website_configuration" "frontend" {
 }
 
 resource "aws_s3_bucket_object" "frontend" {
-  for_each = fileset("../build/", "**/*.*")
-  bucket = aws_s3_bucket.frontend.bucket
-  key = each.value
-  source = "../build/${each.value}"
-  etag = filemd5("../build/${each.value}")
-  content_type = lookup(var.mime_types, split(".", each.value)[length(split(".", each.value)) -1])
+  for_each     = fileset("../build/", "**/*.*")
+  bucket       = aws_s3_bucket.frontend.bucket
+  key          = each.value
+  source       = "../build/${each.value}"
+  etag         = filemd5("../build/${each.value}")
+  content_type = lookup(var.mime_types, split(".", each.value)[length(split(".", each.value)) - 1])
 }
