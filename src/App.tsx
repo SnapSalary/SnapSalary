9089f7d222ff57c32fdf4a76037f4b266d9cdbd2
PDXChristian <8883699+PDXChristian@users.noreply.github.com>
2022-05-04 00:41:28 -0700

    Added small placeholder frontend

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {ReactComponent as Logo} from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo />
+        <p>
+          Future home of SnapSalary. Come find your next job!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
