0f5ada2147226287495b9841926beab17884eeb1
PDXChristian <8883699+PDXChristian@users.noreply.github.com>
2022-05-04 11:53:17 -0700

    Added mime-types

--- tf/variables.tf
+++ tf/variables.tf
@@ -3,3 +3,21 @@ variable "AWS_S3_BUCKET" {
   description = "snapsalary-frontend"
   default     = "snapsalary-frontend"
 }
+
+variable "mime_types" {
+  default = {
+    htm   = "text/html"
+    html  = "text/html"
+    css   = "text/css"
+    ttf   = "font/ttf"
+    js    = "application/javascript"
+    map   = "application/javascript"
+    json  = "application/json"
+    ico   = "image/x-icon"
+    svg   = "image/svg+xml"
+    map   = "application/json"
+    jpg   = "image/jpeg"
+    jpeg  = "image/jpeg"
+    png   = "image/png"
+  }
+}
