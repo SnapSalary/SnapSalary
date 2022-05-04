c4275bbf8266d297165d2b6859d21d504551bbfc
PDXChristian <8883699+PDXChristian@users.noreply.github.com>
2022-05-04 02:57:25 -0700

    Added basic s3 terraform config

---
+++ tf/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~> 3.0"
+    }
+  }
+
+  provider "aws" {
+    region = "us-west-2"
+  }
+
+  resource "aws_s3_bucket_website_configuration" "frontend" {
+    bucket = snapsalary-frontend
+    index_document {
+      suffix = "index.html"
+    }
+  }
+}
