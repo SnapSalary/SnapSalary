455206bea4916654ee93c803056c10153f592b5d
ActIII03 <a.touche03@gmail.com>
2022-06-04 20:04:59 -0700

    small progress

--- server/src/routes/routes.test.ts
+++ server/src/routes/routes.test.ts
@@ -8,20 +8,72 @@
 /* eslint-disable no-unused-vars */
 import request from 'supertest';
 import app from "../server";
-import AWS from "../../node_modules/aws-sdk/";
-import AWSMock from "../../node_modules/aws-sdk-mock";
-import { } from "../../node_modules/aws-sdk/clients/rds";
 
-AWSMock.setSDKInstance(AWS);
+/*
 
+// ISSUE: Run tests back-to-back and/or more w/o fails (setup & teardown)
+
+// ref: https://github.com/dwyl/aws-sdk-mock
+
+import AWSMock from 'aws-sdk-mock';
+import AWS from 'aws-sdk';
+import { GetItemInput } from '../../node_modules/aws-sdk/clients/rds'; // <-- or .../rds
+
+beforeAll(async (done) => {
+  //get requires env vars
+  // inject this into routes?? Don't know how or what else to setup
+  done();
+ });
+
+afterAll(async (done) =>{
+  // tear down
+  done();
+});
+
+describe('the module', () => {
+
+    // TESTS here
+
+  it('should mock getItem from DynamoDB', async () => {
+    // Overwriting DynamoDB.getItem()
+    AWSMock.setSDKInstance(AWS);
+    // Would like: AWSMock.mock('RDS', 'putItem, ...) =>{} ????
+    AWSMock.mock('DynamoDB', 'getItem', (params: GetItemInput, callback: Function) => {
+      console.log('DynamoDB', 'getItem', 'mock called');
+      callback(null, {pk: 'foo', sk: 'bar'});
+    })
+
+    const input:GetItemInput = { TableName: '', Key: {} };
+    const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+    expect(await dynamodb.getItem(input).promise()).toStrictEqual({ pk: 'foo', sk: 'bar' });
+
+    AWSMock.restore('DynamoDB');
+  });
+
+  it('should mock reading from DocumentClient', async () => {
+    // Overwriting DynamoDB.DocumentClient.get()
+    AWSMock.setSDKInstance(AWS);
+    AWSMock.mock('DynamoDB.DocumentClient', 'get', (params: GetItemInput, callback: Function) => {
+      console.log('DynamoDB.DocumentClient', 'get', 'mock called');
+      callback(null, {pk: 'foo', sk: 'bar'});
+    });
+
+    const input:GetItemInput = { TableName: '', Key: {} };
+    const client = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+    expect(await client.get(input).promise()).toStrictEqual({ pk: 'foo', sk: 'bar' });
+
+    AWSMock.restore('DynamoDB.DocumentClient');
+  });
+});
+
+*/
+
+
+
+/*
+// NOTE: Previous tests
 // Tasks before: (i) re-init table (seeding from doggr for instance)
 describe("POST /company", () => {
-  beforeAll(async () => {
-    AWSMock.mock("RDS", "createDBInstance", ); 
-  })
-
-
-
   describe("when passed company type", () => {
     test("should respond w/ HTTP Status code(200)",
       async () => {
@@ -36,43 +88,50 @@ describe("POST /company", () => {
   })
 });
 
-//describe("GET /company", () => {
-//  describe("when passed company type", () => {
-//    test("should respond w/ HTTP Status code(200)",
-//      async () => {
-//        const response = await request(app).get('/company').send({
-//          company_name: "A",
-//          state: "OR",
-//          country: "US",
-//          industry_id: "1",
-//        })
-//
-//        expect(response.statusCode).toBe(200);
-//      })
-//  })
-//});
-//
-//describe("DELETE /company", () => {
-//  describe("when passed company type", () => {
-//    test("should respond w/ HTTP Status code(200)",
-//      async () => {
-//        const response = await request(app).delete('/company').send({
-//          company_id: "1",
-//        })
-//        expect(response.statusCode).toBe(200);
-//      })
-//  })
-//});
-//
-//describe("GET /industry", () => {
-//  describe("when passed industry type", () => {
-//    test("should respond w/ HTTP Status code(200)",
-//      async () => {
-//        const response = await request(app).get('/company').send({
-//          industry: "A",
-//        })
-//        expect(response.statusCode).toBe(200);
-//      })
-//  })
-//});
-//
+describe("GET /company", () => {
+  describe("when passed company type", () => {
+    test("should respond w/ HTTP Status code(200)",
+      async () => {
+        const response = await request(app).get('/company').send({
+          company_name: "A",
+          state: "OR",
+          country: "US",
+          industry_id: "1",
+        })
+        // clean up data
+        const temp = await await request(app).delete('/company').send({
+          company_name: "A",
+          state: "OR",
+          country: "US",
+          industry_id: "1",
+        })
+        expect(response.statusCode).toBe(200);
+      })
+  })
+});
+
+describe("DELETE /company", () => {
+  describe("when passed company type", () => {
+    test("should respond w/ HTTP Status code(200)",
+      async () => {
+        const response = await request(app).delete('/company').send({
+          company_id: "1",
+        })
+        expect(response.statusCode).toBe(200);
+      })
+  })
+});
+
+describe("GET /industry", () => {
+  describe("when passed industry type", () => {
+    test("should respond w/ HTTP Status code(200)",
+      async () => {
+        const response = await request(app).get('/company').send({
+          industry: "A",
+        })
+        expect(response.statusCode).toBe(200);
+      })
+  })
+});
+
+*/
